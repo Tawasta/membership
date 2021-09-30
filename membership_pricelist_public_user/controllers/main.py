59d0ff5ec98864ff2a64d97827c3054bf4a5cc0d
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2021-09-30 08:17:18 +0300

    Modified function

--- membership_pricelist_public_user/controllers/main.py
+++ membership_pricelist_public_user/controllers/main.py
@@ -26,14 +26,22 @@ class WebsiteSale(WebsiteSale):
         order = request.website.sale_get_order(force_create=True)
         use_membership_pricelist = False
         if order.partner_id.id == request.website.user_id.sudo().partner_id.id:
-            pricelist_context, pricelist = self._get_pricelist_context()
+            # pricelist_context, pricelist = self._get_pricelist_context()
+            # pricelist_price = order.pricelist_id.with_context(uom=option.uom_id.id).get_product_price(option.product_id, 1, False)
 
-            print(pricelist)
+            # print(pricelist)
             for line in order.order_line:
                 if line.product_id.membership:
+                    price_unit = line.order_id.pricelist_id.get_product_price(
+                        line.product_id,
+                        add_qty,
+                        line.order_id.partner_id,
+                        uom_id=line.product_id.uom_id.id,
+                    )
                     use_membership_pricelist = True
                     current_line = line
 
+            print(price_unit)
             if use_membership_pricelist:
                 membership_pricelist = (
                     request.env["product.pricelist"]
@@ -51,36 +59,34 @@ class WebsiteSale(WebsiteSale):
                         force_pricelist=membership_pricelist.id
                     )
                     # public_pricelist = order.partner_id.property_product_pricelist
-                    public_items = (
-                        request.env["product.pricelist.item"]
-                        .sudo()
-                        .search(
-                            [
-                                ("pricelist_id", "=", pricelist.id),
-                                ("product_id", "=", current_line.product_id.id),
-                            ]
-                        )
-                    )
-                    if public_items:
-                        find_date_item = False
-                        date = datetime.now()
-                        for item in public_items:
-                            if item.date_start and item.date_end:
-                                if item.date_start <= date <= item.date_end:
-                                    find_date_item = True
-                                    current_line.sudo().write(
-                                        {"price_unit": item.fixed_price}
-                                    )
-                        if find_date_item is False:
-                            for item in public_items:
-                                if not item.date_start and not item.date_end:
-                                    current_line.sudo().write(
-                                        {"price_unit": item.fixed_price}
-                                    )
-                    else:
-                        current_line.sudo().write(
-                            {"price_unit": current_line.product_id.lst_price}
-                        )
+                    # public_items = (
+                    #     request.env["product.pricelist.item"]
+                    #     .sudo()
+                    #     .search(
+                    #         [
+                    #             ("pricelist_id", "=", pricelist.id),
+                    #             ("product_id", "=", current_line.product_id.id),
+                    #         ]
+                    #     )
+                    # )
+                    # if public_items:
+                    #     find_date_item = False
+                    #     date = datetime.now()
+                    #     for item in public_items:
+                    #         if item.date_start and item.date_end:
+                    #             if item.date_start <= date <= item.date_end:
+                    #                 find_date_item = True
+                    #                 current_line.sudo().write(
+                    #                     {"price_unit": item.fixed_price}
+                    #                 )
+                    #     if find_date_item is False:
+                    #         for item in public_items:
+                    #             if not item.date_start and not item.date_end:
+                    #                 current_line.sudo().write(
+                    #                     {"price_unit": item.fixed_price}
+                    #                 )
+                    # else:
+                    current_line.sudo().write({"price_unit": price_unit})
 
             return res
 
