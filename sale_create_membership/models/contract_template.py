4fbeec27050b86be1bb1aa875321d6c4cb560aaa
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2021-10-11 09:12:05 +0300

    Remove fields

--- sale_create_membership/models/contract_template.py
+++ sale_create_membership/models/contract_template.py
@@ -9,5 +9,9 @@ class ContractTemplate(models.Model):
             fields = contract_template._fields.keys()
             vals = self._convert_to_write(contract_template.read(fields)[0])
             vals.pop("id", None)
-
+            vals.pop("create_uid", None)
+            vals.pop("create_date", None)
+            vals.pop("write_uid", None)
+            vals.pop("write_date", None)
+            vals.pop("__last_update", None)
         return vals
