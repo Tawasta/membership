672c7a7066e0c14d976aecbde01f702fcbbd51ae
Miika Nissi <miika@miikanissi.com>
2022-01-17 17:02:13 +0200

    [IMP] Add search for partner in membershop line view

--- membership_line_view/__manifest__.py
+++ membership_line_view/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Membership Line View",
     "summary": "Membership Line View",
-    "version": "14.0.1.0.3",
+    "version": "14.0.1.0.4",
     "category": "Sales",
     "website": "https://gitlab.com/tawasta/odoo/membership/",
     "author": "Tawasta",
