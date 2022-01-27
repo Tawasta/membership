c0c50ef8a625b463c7bec1234499634826f9a491
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2022-01-27 12:49:21 +0200

    Added function to modify partner subtype

--- sale_create_membership/models/contract.py
+++ sale_create_membership/models/contract.py
@@ -1,7 +1,19 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ContractContract(models.Model):
     _inherit = "contract.contract"
 
     note = fields.Html('Terms and conditions')
+
+    @api.model
+    def _set_start_contract_modification(self):
+        contract = super()._set_start_contract_modification()
+        subtype_id = self.env['mail.message.subtype'].sudo().search([('name', '=', 'Discussions')])
+        if subtype_id and self.message_follower_ids:
+            for follower in self.message_follower_ids:
+                if follower.partner_id == self.partner_id:
+                    follower.sudo().write({
+                        'subtype_ids': [[6, 0, [subtype_id[0].id]]]
+                    })
+        return contract
