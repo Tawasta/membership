ecc64ac528b5f3304499259ce7935acde0b7a07b
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2022-08-29 14:11:32 +0300

    Modified function

---
+++ membership_group/models/account_move.py
@@ -0,0 +1,22 @@
+from odoo import fields, models
+
+
+class AccountMove(models.Model):
+
+    _inherit = "account.move"
+
+    def _get_invoice_in_payment_state(self):
+        res = super()._get_invoice_in_payment_state()
+
+        if res == "paid":
+            print(self)
+            is_membership = False
+
+            for line in self.invoice_line_ids:
+                if line.product_id.membership:
+                    is_membership = True
+
+            if is_membership:
+                self.partner_id._compute_membership_state()
+
+        return res
