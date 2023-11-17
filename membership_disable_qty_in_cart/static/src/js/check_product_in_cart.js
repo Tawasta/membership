b9db06866dff8459c1e7fc7d9d619df00950efff
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-11-17 11:15:11 +0200

    Modified js

--- membership_disable_qty_in_cart/static/src/js/check_product_in_cart.js
+++ membership_disable_qty_in_cart/static/src/js/check_product_in_cart.js
@@ -5,24 +5,36 @@ odoo.define('membership_disable_qty_in_cart.product_cart_check', function (requi
 
     $(document).ready(function () {
         var $button = $('#add_to_cart');
-        if ($button.length) {
-            var productId = $button.closest('.oe_website_sale').find('.product_id').val();
-            console.log(productId);
-            checkIfProductInCart(productId, function (isInCart) {
-                console.log(isInCart);
-                if (isInCart) {
-                    $("#add_to_cart").addClass("blocked");
-                    $("#buy_now").addClass("blocked");
-                }
-            });
+        checkProductAndBlockIfNeeded($button);
+
+        $('.variant_attribute select').on('change', function () {
+            checkProductAndBlockIfNeeded($button);
+        });
+
+        function checkProductAndBlockIfNeeded($button) {
+            if ($button.length) {
+                var productId = $button.closest('.oe_website_sale').find('.product_template_id').val();
+                checkIfProductInCart(productId, function (isInCart) {
+                    if (isInCart) {
+                        $("#add_to_cart").addClass("blocked");
+                        $("#buy_now").addClass("blocked");
+                    } else {
+                        $("#add_to_cart").removeClass("blocked");
+                        $("#buy_now").removeClass("blocked");
+                    }
+                });
+            }
         }
 
         function checkIfProductInCart(productId, callback) {
             // AJAX-kutsu Odoo-palvelimelle
             ajax.jsonRpc('/check_product_in_cart', 'call', {'product_id': productId})
-                .then(function (response) {
+                .done(function (response) {
                     callback(response.in_cart);
                 })
+                .fail(function () {
+                    callback(false);
+                });
         }
     });
 });
