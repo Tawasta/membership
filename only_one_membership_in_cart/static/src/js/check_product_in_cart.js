2df395de242d1cb60ebe84388915c47d08b8691e
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-11-20 08:18:26 +0200

    Modified js

--- only_one_membership_in_cart/static/src/js/check_product_in_cart.js
+++ only_one_membership_in_cart/static/src/js/check_product_in_cart.js
@@ -4,45 +4,48 @@ odoo.define('only_one_membership_in_cart.product_cart_check', function (require)
     var ajax = require('web.ajax');
     var checkProductAvailability = require('product_cant_order.product').checkProductAvailability;
 
+    function checkProductAndBlockIfNeeded($button) {
+        if ($button.length) {
+            var productId = $('.product_id').val(); // Haetaan productId .product_id kentästä
+
+            checkProductAvailability(productId, function (availabilityResult) {
+                checkIfProductInCart(productId, function (isInCart) {
+                    if (isInCart) {
+                        $("#add_to_cart").addClass("blocked");
+                        $("#buy_now").addClass("blocked");
+                    } else {
+                        $("#add_to_cart").removeClass("blocked");
+                        $("#buy_now").removeClass("blocked");
+                    }
+                });
+            });
+        }
+    }
+
+    function checkIfProductInCart(productId, callback) {
+        ajax.jsonRpc('/check_product_in_cart', 'call', {'product_id': productId})
+            .then(function (response) {
+                callback(response.in_cart);
+            });
+    }
+
     $(document).ready(function () {
         var $button = $('#add_to_cart');
         checkProductAndBlockIfNeeded($button);
 
-        $('.variant_attribute select').on('change', function () {
-            // Asetetaan pieni viive varmistaaksemme, että arvo on ehtinyt vaihtua
-            setTimeout(function() {
-                checkProductAndBlockIfNeeded($button);
-            }, 250);
+        // Tapahtumankäsittelijä .product_id kentälle
+        $('.product_id').on('change', function() {
+            checkProductAndBlockIfNeeded($button);
         });
-
-
-        function checkProductAndBlockIfNeeded($button) {
-            if ($button.length) {
-                var productId = $button.closest('.oe_website_sale').find('.product_id').val();
-
-                // Ensin suoritetaan peritty checkProductAvailability-funktio
-                checkProductAvailability(productId, function (availabilityResult) {
-                    // Sitten tämän moduulin toiminnallisuus
-                    checkIfProductInCart(productId, function (isInCart) {
-                        if (isInCart) {
-                            $("#add_to_cart").addClass("blocked");
-                            $("#buy_now").addClass("blocked");
-                        } else {
-                            $("#add_to_cart").removeClass("blocked");
-                            $("#buy_now").removeClass("blocked");
-                        }
-                    });
-                });
-            }
-        }
-
-        function checkIfProductInCart(productId, callback) {
-            ajax.jsonRpc('/check_product_in_cart', 'call', {'product_id': productId})
-                .then(function (response) {
-                    callback(response.in_cart);
-                });
-        }
     });
+
+    // Oletettu funktio, joka päivittää .product_id:n arvon
+    function updateProductId(newProductId) {
+        $('.product_id').val(newProductId).trigger('change');
+    }
+
+    return {
+        checkProductAvailability: checkProductAvailability,
+        updateProductId: updateProductId
+    };
 });
-
-
