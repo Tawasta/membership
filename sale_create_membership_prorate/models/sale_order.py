3d4c36e838ecb80ff11c6f0653636fb55ddeee1d
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-06-04 12:57:01 +0300

    [FIX] Membership price prorate from eCommerce

--- sale_create_membership_prorate/models/sale_order.py
+++ sale_create_membership_prorate/models/sale_order.py
@@ -9,6 +9,17 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    def _cart_update(self, *args, **kwargs):
+        res = super()._cart_update(*args, **kwargs)
+
+        sale_order_line = self.env['sale.order.line'].sudo()
+        sol_id = res.get("line_id")
+        qty = res.get("quantity", 0)
+        if sol_id and qty:
+            sale_order_line.browse([sol_id]).product_id_change()
+
+        return res
+
     def _calculate_last_date_invoiced(self):
         last_date_invoiced = self._get_existing_membership_last_date_invoiced()
         if last_date_invoiced:
@@ -49,7 +60,12 @@ class SaleOrder(models.Model):
         self.ensure_one()
 
         last_date = self._get_existing_membership_last_date_invoiced()
-        days_difference = last_date - fields.Date().today()
+        date_today = fields.Date().today()
+
+        if not last_date:
+            last_date = date_today
+
+        days_difference = last_date - date_today
         discount = 0
 
         # TODO: configurable method (days, month)
@@ -60,8 +76,14 @@ class SaleOrder(models.Model):
         if prorate_method == "month":
             period_name = _("months")
             period = days_difference.days / 30
+
             if prorate_rounding == "ceil":
                 period = math.ceil(period)
+
+            if period > 11:
+                # Pre-invoicing over 11 months is not supported
+                period = 0
+
             discount = 100 / 12 * period
             if discount < 0:
                 # If the next invoice date is in the past, we'll get a negative discount
