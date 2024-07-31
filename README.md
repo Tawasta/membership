d00c00f243d70a519735e443e2c8f0f0f28dedc2
difbit <timo.kekalainen@tawasta.fi>
2024-07-31 09:15:55 +0300

    [MIG] Migration of membership_group module from 14.0 to 17.0 version

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ addon | version | maintainers | summary
 --- | --- | --- | ---
 [membership_contract_menu](membership_contract_menu/) | 17.0.1.0.0 |  | Membership contract menu
 [membership_disable_qty_in_cart](membership_disable_qty_in_cart/) | 17.0.1.0.0 |  | Disable changing membership product quantity in cart
+[membership_group](membership_group/) | 17.0.1.0.0 |  | Add active members to a membership group
 [membership_product](membership_product/) | 17.0.1.0.0 |  | Membership Product
 [membership_website_header_boxed_button](membership_website_header_boxed_button/) | 17.0.1.0.0 |  | Replaces 'Contact Us' menu from website with Membership links
 [only_one_membership_in_cart](only_one_membership_in_cart/) | 17.0.1.0.0 |  | Allow to have only one membership in cart
---
+++ membership_group/models/groups.py
@@ -0,0 +1,38 @@
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class ResGroups(models.Model):
+    # 1. Private attributes
+    _inherit = "res.groups"
+
+    # 2. Fields declaration
+    membership_group = fields.Boolean(
+        string="Is a membership group",
+        default=False,
+    )
+
+    # 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
+
+    # 5. Constraints and onchanges
+    @api.onchange("membership_group")
+    def _onchange_membership_group(self):
+        if self.membership_group:
+            already_selected = (
+                self.env["res.groups"].sudo().search([("membership_group", "=", True)])
+            )
+            if already_selected:
+                raise ValidationError(
+                    _(
+                        "You cannot define that group as a membership "
+                        "because another group already has permission."
+                    )
+                )
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
